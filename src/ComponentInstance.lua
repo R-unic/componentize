e2b85c01cce348401ff193df256d002156f2fcb8
AlphaRunic <roleyspersonal@gmail.com>
2023-09-21 12:28:48 -0400

    fix: janitor import + add ServiceEventLinker

--- src/ComponentInstance.lua
+++ src/ComponentInstance.lua
@@ -1,8 +1,6 @@
 --!native
 --!strict
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local Janitor = require(ReplicatedStorage.Packages.janitor)
+local Janitor = require(script.Parent.Parent.Parent.janitor)
 local ServiceEventLinker = require(script.Parent.ServiceEventLinker)
 local Types = require(script.Parent.Types)
 
